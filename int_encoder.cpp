8e77666960ddfdead545d13e9b4a538bdf671cee
Aumann- <zaiaku01@gmail.com>
2013-10-02 14:18:32 -0700

    Changed to use functions
    Changed code to use functions instead of being all in main.

--- int_encoder.cpp
+++ int_encoder.cpp
@@ -2,18 +2,31 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+void Base(int, int);
+void Encode(int, int);
+
+
 int main()
 {
 	srand(time(NULL));
-	int i = 0;
 	int key = 4;
 	int shift = rand() % 100;
-	//cout << i << endl;
 	
 	//gets a key value (amount of sets to output
 	cout << "Enter a key-value: " << endl;
 	cin >> key;
 	
+	Base(key, shift);
+	Encode(key, shift);
+	
+	cin.ignore();
+	cin.ignore();
+}
+
+void Base(int key, int shift)
+{
+	int i = 0;
 	//encodes based on 0
 	cout << "Base Encoding" << endl;
 	i += shift;
@@ -24,7 +37,11 @@ int main()
 		++i;
 	}
 	cout << i-- << i++ << endl;
-	
+}
+
+void Encode(int key, int shift)
+{
+	int i;
 	//gets a number and encodes it based on the key
 	cout << "Enter a number: ";
 	cin >> i;
@@ -38,7 +55,7 @@ int main()
 		++i;
 	}
 	cout << i-- << i++ << endl;
-	//cout << i++ << " " << ++i << " " << i++ << " " << ++i << " " << i++ << " " << ++i << " " << i++ << " " << ++i << endl;
-	cin.ignore();
-	cin.ignore();
 }
+
+
+
